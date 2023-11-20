2bfe18d984ed258e4aed7b218fc62657aa0389b6
Bapun <learnwithbapun@gmail.com>
2023-11-20 22:15:01 +0530

    Added

---
+++ src/com/Rectangle.java
@@ -0,0 +1,19 @@
+package com;
+
+public class Rectangle {
+
+        int height;
+        int width;
+
+        void setHeight(int ht) {
+                height = ht;
+        }
+
+        void setWidth(int w) {
+                width = w;
+        }
+
+        int getArea() {
+                return height * width;
+        }
+}
